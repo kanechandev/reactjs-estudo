0f8e5cdc27dad5d69c56341c1effb7939068e191
cursoskanechan <kanes.chan@gmail.com>
2022-06-14 14:19:07 -0300

    feat: contador com state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const[n, setN] = useState(0);
+
+  const handleMinus = () => {
+    setN(n-1);
+  }
+
+  const handleMore = () => {
+    setN(n+1);
+  }
+
+  return(
+    <div>
+      <button onClick={handleMinus}>-</button>
+      <div>
+        {n}
+      </div>
+      <button onClick={handleMore}>+</button>
     </div>
   );
 }
