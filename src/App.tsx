dab0e2e85e030dcea7efa473abd8c116fbf586b8
cursoskanechan <kanes.chan@gmail.com>
2022-06-14 15:29:48 -0300

    feat: state com vários inputs e change.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const[nome, setNome] = useState('Kane');
+  const[nome, setNome] = useState('');
+  const[sobrenome, setSobrenome] = useState('');
+  const[idade, setIdade] = useState('');
 
-  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handleInputNome = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNome(event.target.value);
+  }
+
+  const handleInputSobrenome = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSobrenome(event.target.value);
+  }
+
+  const handleInputIdade = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIdade(event.target.value);
   }
 
   return(
     <div>
-      Nome:
-      <input type="text" value={nome}  onChange={handleInput}/>
-      <hr />
-      Seu nome é: {nome}
+      <div>
+        Nome:
+        <input type="text" value={nome}  onChange={handleInputNome}/>
+      </div>
+      <div>
+        Sobrenome:
+        <input type="text" value={sobrenome}  onChange={handleInputSobrenome}/>
+      </div>
+      <div>
+        Idade:
+        <input type="text" value={idade}  onChange={handleInputIdade}/>
+      </div>
+      <hr/>
+      Olá {nome} {sobrenome}, tudo bem?<br/>
+      Você tem {idade} anos.
     </div>
   );
 }
