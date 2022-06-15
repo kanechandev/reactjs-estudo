342369a3262d4528c1b53b83eb70ab73c225aedd
cursoskanechan <kanes.chan@gmail.com>
2022-06-15 15:05:33 -0300

    feat: CSS inline dinâmico.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 
 const App = () => {
+  const [color, setColor] = useState('#ff0000');
+  const [padding, setPadding] = useState(0);
+
+  const handleClick = () => {
+    setColor('#1100ff');
+    setPadding(20);
+  }
 
   return(
     <div>
-      <button style={{ 
-          background: '#1100FF', 
-          color: '#000000', 
-          border: '0'}}>
+      <button onClick={handleClick} style={{ 
+          background: color , color: '#000000', 
+          border: '0', padding}}>
         Clique aqui!
       </button>
-    </div>
+    </div> 
   );
 }
 
