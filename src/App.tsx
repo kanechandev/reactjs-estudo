2fd9b27eb76436667b2f86e9cb77f2d2ad264b96
cursoskanechan <kanes.chan@gmail.com>
2022-06-16 13:29:52 -0300

    feat: Aplicando stylesheet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { useState } from "react";
+import { Botao } from "./components/Botao";
+
 
 const App = () => {
-  const [color, setColor] = useState('#ff0000');
-  const [padding, setPadding] = useState(0);
-
-  const handleClick = () => {
-    setColor('#1100ff');
-    setPadding(20);
-  }
-
   return(
     <div>
-      <button onClick={handleClick} style={{ 
-          background: color , color: '#000000', 
-          border: '0', padding}}>
-        Clique aqui!
-      </button>
+      <Botao/>
     </div> 
   );
 }
---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export const Botao = () => {
+    return(
+        <div className='botao-box'>
+            <button className="botao-btn">Lorem ipsum</button>
+            <p className='botao-legenda'>Legenda</p>
+        </div>
+    );
+}
