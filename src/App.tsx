d7ef8758be26997d4cf437ca51de0036837c3c8b
cursoskanechan <kanes.chan@gmail.com>
2022-06-16 21:47:20 -0300

    feat: styled com props dinâmicas.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import * as C from './AppStyles';
 
 const App = () => {
     return(
-        <C.Container>
+        <C.Container bgColor="#1c1c1c">
             Lorem ipsum
-            <C.Botao>Clique</C.Botao>
+            <C.Botao bg="#ff0000">Clique</C.Botao>
+            <C.Botao bg="#00ff00" small>Clique</C.Botao>
         </C.Container>
     );
 }
--- src/AppStyles.ts
+++ src/AppStyles.ts
@@ -1,11 +1,21 @@
 import styled from "styled-components";
 
-export const Botao = styled.button`
-    font-size: 40px;
+type BotaoProps = {
+    bg: string;
+    small?: boolean;
+}
+
+export const Botao = styled.button<BotaoProps>`
+    font-size: ${(props) => props.small ? '15px' : '30px'};
+    background-color: ${props => props.bg}
 `;
 
-export const Container = styled.div`    
-    background-color: blue;
+type ContainerProps = {
+    bgColor: string;
+}
+
+export const Container = styled.div<ContainerProps>`    
+    background-color: ${(props) => props.bgColor};
     color: white;
     padding: 20px;
 `;
